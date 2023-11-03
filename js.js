aecc9a4eef29f8e9f50db289e641ae879ed4bbd1
Valdemar <valdemar.kobelis@gmail.com>
2023-11-03 10:08:16 +0200

    Add form that will let users input their location and will fetch the weather info

--- js.js
+++ js.js
@@ -1,11 +1,17 @@
-async function getWeather(location) {
+async function getWeatherPromise(location) {
   const response = await fetch('https://api.weatherapi.com/v1/current.json?key=7dcc585f30c04aaeab9154430233010&q=' + location, {mode: 'cors'})
   const data = await response.json()
-  return [data.current.condition.text, data.current.temp_c]
+  return {text: data.current.condition.text, temp_c: data.current.temp_c}
 }
 
-async function getGiphy(text) {
-  const reponse = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=R3bOn6Qg6iJKatLcEiXo5jgjmb3qVkKa&s=' + text, {mode: 'cors'})
+async function getGiphyPromise(text) {
+  const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=R3bOn6Qg6iJKatLcEiXo5jgjmb3qVkKa&s=' + text, {mode: 'cors'})
   const data = await response.json()
-  return response.data.images.original.url
+  return data.data.images.original.url
 }
+
+const button = document.querySelector("button")
+button.addEventListener("click", () => {
+  const location = document.querySelector('#location').value
+  getWeatherPromise(location).then(a => {console.log(a)})
+})
